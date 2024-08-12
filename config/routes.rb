08a7e0ff616bb10258dc029db18e23d4e81b45b0
hie3-336 <tomohiko336@gmail.com>
2024-08-12 18:51:40 +0900

    bootstrapを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/show'
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'items#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
