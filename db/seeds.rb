a801c98ee90e82fad50866b529bd9345507e4dbb
hie3-336 <tomohiko336@gmail.com>
2024-08-17 17:26:09 +0900

    seedsを書いてみる

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+item = Item.create(name: 'テスト商品', description: 'テストの説明です！テストの説明です！', price: '1800円', number: 'TEST-01')
+
+item.image.attach(io: File.open('/storage/test.png'))
+item.save!
