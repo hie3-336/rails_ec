568bf2270577361b57aa804f15e4deb1419802e7
hie3-336 <tomohiko336@gmail.com>
2024-08-25 11:03:21 +0900

    seedを実行できるよう環境整備

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,19 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-item = Item.create(name: 'テスト商品', description: 'テストの説明です！テストの説明です！', price: '1800円', number: 'TEST-01')
+# item = Item.create(name: 'テスト商品', description: 'テストの説明です！テストの説明です！', price: '1800円', number: 'TEST-01')
 
-item.image.attach(io: File.open('db/testimage/test.png'), filename: 'test.png')
-item.save!
+# item.image.attach(io: File.open('db/testimage/test.png'), filename: 'test.png')
+# item.save!
+require "csv"
+
+CSV.foreach('db/seeds.csv', headers: true) do |row|
+  item = Item.create(
+    name: row['name'],
+    description: row['description'],
+    price: row['price'],
+    number: row['number']
+  )
+  item.image.attach(io: File.open("db/testimage/#{row['filename']}.jpg"), filename: "#{row['filename']}.png")
+  item.save
+end
