916bd48c450f88ab46f08c890e34db502b34ec66
hie3-336 <tomohiko336@gmail.com>
2024-08-25 23:46:18 +0900

    一旦showアクションで商品詳細ページに飛ぶように設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/show'
+  resources :items, only: [:index, :show]
+  # get 'items', to: 'items#index'
+  # get 'items/:id', to: 'items#show'
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'items#index'
