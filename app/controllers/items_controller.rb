d487fe392b401d90c439a40fe6c2c2cd588daf96
hie3-336 <tomohiko336@gmail.com>
2024-09-02 00:35:32 +0900

    商品一覧表示のN+1問題に対応

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,12 @@
 
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.with_attached_image
+    # @items = Item.all
   end
 
   def show
     @item = Item.find(params[:id])
-    @items = Item.all
+    @items = Item.with_attached_image
   end
 end
