add43c01d7add1b85d26e6813db94b3987ae437e
hie3-336 <tomohiko336@gmail.com>
2024-09-08 20:11:38 +0900

    Itemのpriceに数値限定のバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
   validates :number, presence: true
   validates :image, presence: true
 end
