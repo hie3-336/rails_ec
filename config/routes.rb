4e08db1d4e07e6afbce37ab5f4b90dc93cb1aa7c
hie3-336 <tomohiko336@gmail.com>
2024-09-21 10:08:28 +0900

    一旦カートページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'carts/index'
+  get 'carts/create'
+  get 'carts/destroy'
   resources :items, only: %i[index show]
 
   namespace :admin do
     resources :items, except: %i[show]
   end
+
+  resources :carts, only: %i[index create destroy]
 
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
