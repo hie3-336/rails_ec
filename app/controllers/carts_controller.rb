4e2173cc25027b7ccc1c55eeec8a85ff2b634a2c
hie3-336 <tomohiko336@gmail.com>
2024-09-28 01:58:08 +0900

    カート削除機能追加

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,32 +1,31 @@
 class CartsController < ApplicationController
 
-  # before_action :setup_cart_item
+  before_action :setup_cart_item , only: %i[add_item]
 
   def show
   end
 
   def my_cart
     @cart_items = current_cart.cart_items.includes(:item)
-
   end
 
   def add_item
-    if current_cart.cart_items.find_by(item_id: params[:item_id])
-      @cart_item = current_cart.cart_items.find_by(item_id: params[:item_id])
-    else
-      @cart_item = current_cart.cart_items.build(item_id: params[:item_id])
-    end
-
+    @cart_item ||= current_cart.cart_items.build(item_id: params[:item_id])
     @cart_item.count ||= 0
     @cart_item.count += params[:add_item_count].to_i
-
     @cart_item.save
+
   end
 
   def create
   end
 
-  def destroy
+  def delete_cart
+    if current_cart.destroy
+      redirect_to my_cart_path, notice: "カートの中身を削除しました。"
+    else
+      redirect_to my_cart_path, notice: "カート削除に失敗しました。"
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   root 'items#index'
   post '/add_item/:item_id', to: 'carts#add_item', as: :add_item
   get '/my_cart', to: 'carts#my_cart'
+  get '/delete_cart', to: 'carts#delete_cart'
   # Defines the root path route ("/")
   # root "articles#index"
 end
